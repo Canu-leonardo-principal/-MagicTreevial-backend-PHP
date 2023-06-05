a94a1caf7997ec67c7caf5e411143a8e5ee111ea
MeatyMage46 Canu <canu.leonardo@itismeucci.com>
2023-06-05 21:51:32 +0200

    in teria funziona

--- index.php
+++ index.php
@@ -10,33 +10,45 @@ use Symfony\Component\HttpFoundation\Response;
 $router = new Router;
 
 $router->get('/start', function (Request $request, Response $response) {
+    //settiamo i parametri dell'header di risposta
+    $response -> headers -> set('Access-Control-Allow-Origin', '*');
     $response -> headers -> set('Content-Type', 'application/json');
+    //creaiamo il puzzle
     $puzzle = new Puzzle($request->get('seed'));
+    //creaiamo la risposta
     $response->setContent(json_encode($puzzle->get_lengths()));
-    return $response;    
+    return $response;
 });
 
 
-$router->get('/verify', function (Request $request, Response $response) {
+$router->post('/verify', function (Request $request, Response $response) {
+    //settiamo i parametri dell'header di risposta
+    $response -> headers -> set('Access-Control-Allow-Origin', '*');
     $response -> headers -> set('Content-Type', 'application/json');
+    //ricreaiamo il puzzle
     $puzzle = new Puzzle($request->get('seed'));
-    
-    //array di prova, da sostituire con quello inviato dal client
-    $words = array(                            
-        array('s', ' ', 'g', 'n', 'o', 'r', 'e'),
-        array('c', 'o', 'n', 't', 'e'),
-    );
+
+    $words = json_decode($request->getContent());
+
+    $error = array();
 
     for ($i = 0; $i < count($words); $i++) {
         for ($j = 0; $j < count($words[$i]); $j++) {
-            if ( ! $puzzle->isThisLetterTrue($i /*indice parola*/ , $j /*indice lettera*/, $words[$i][$j] /*lettera*/)) {
+            if ( !$puzzle->isThisLetterTrue($i /*indice parola*/ , $j /*indice lettera*/, strval($words[$i][$j]) /*lettera*/)) { 
                 $error[] =  ($i * 100) + $j;
             }
         }
     }
-
     $response->setContent(json_encode($error));
 
+    return $response;
+});
+
+$router->get('/debug', function (Request $request, Response $response){
+    $response->headers->set('Access-Control-Allow-Origin', '*');
+    $response -> headers -> set('Content-Type', 'application/json');
+    $puzzle = new Puzzle($request->get('seed'));
+    $response->setContent(json_encode($puzzle->get_solution()));
     return $response;
 });
 
--- src/word_gen.php
+++ src/word_gen.php
@@ -1,9 +1,12 @@
 <?php
 
+use function PHPSTORM_META\type;
+
 class Puzzle
 {
     public $connection;
     public $seed;
+    public $master_word;
     public $puzzle_words = array();
 
     function __construct($seed)
@@ -24,30 +27,31 @@ class Puzzle
         $row = $result->fetch_assoc();
 
         //parola principale
-        $this->puzzle_words[] = $row['Contenuto'];
+        $this->master_word = $row['Contenuto'];
 
-        for ($i = 0; $i < strlen($this->puzzle_words[0]); $i++)
+        for ($i = 0; $i < strlen($this->master_word); $i++)
         {
             //massimo parole che iniziano con master_word[$i]
-            $query_max = 'SELECT count(*) as max FROM parola WHERE parola.Contenuto LIKE "' . $this->puzzle_words[0][$i] . '%"';
+            $query_max = 'SELECT count(*) as max FROM parola WHERE parola.Contenuto LIKE "' . $this->master_word[$i] . '%"';
             $res_max = $this->connection->query($query_max);
             $max = $res_max->fetch_assoc();
 
             //selezionare una delle parole
-            $num_offset = rand(0, $max["max"]);
-            $word_query = 'SELECT * FROM parola WHERE parola.Contenuto LIKE "' . $this->puzzle_words[0][$i] . '%" LIMIT 1 OFFSET ' . $num_offset;
+            $num_offset = rand(0, $max["max"]-1);
+            $word_query = 'SELECT * FROM parola WHERE parola.Contenuto LIKE "' . $this->master_word[$i] . '%" LIMIT 1 OFFSET ' . $num_offset;
             $word_res = $this->connection->query($word_query);
             $word_row = $word_res->fetch_assoc();
             $this->puzzle_words[] = $word_row['Contenuto'];
+            //echo gettype($word_row['Contenuto']);
         }
     }
 
-    //non prende la lungezza del primo
+    //non prende la lungezza del master
     function get_lengths()
     {
         $arr = array(); 
         
-        for ($i = 1; $i < count($this->puzzle_words); $i++)
+        for ($i = 0; $i < count($this->puzzle_words); $i++)
         {
             $arr[] = strlen($this->puzzle_words[$i]);
         }
@@ -57,7 +61,12 @@ class Puzzle
 
     function get_word($index){  return $this->puzzle_words[$index];  }
     function isThisLetterTrue($index_word, $index_letter, $letter){
-        if ($this->puzzle_words[$index_word+1][$index_letter] === $letter){  return true;  }
+        if (strval($this->puzzle_words[$index_word][$index_letter]) === $letter){  return true;  }
         return false;
+    }
+
+    function get_solution()
+    {
+        return $this->puzzle_words;
     }
 }
